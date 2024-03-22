ce9ff4ebb69d8f455568ce6183161e364a3c705f
Nuwan Vidanapathiranage <nuwanpvp@gmail.com>
2024-03-22 23:37:20 +0530

    refactor: Update Pagination component to conditionally enable Next and Previous buttons based on existence of nextPageUrl and prevPageUrl

--- Pokémon Application/src/App.jsx
+++ Pokémon Application/src/App.jsx
@@ -35,8 +35,8 @@ function App() {
     <>
       <PokemonList pokemon={pokemon}/>
       <Pagination
-        gotoNextPage = {gotoNextPage}
-        gotoPrevPage = {gotoPrevPage}
+        gotoNextPage = {nextPageUrl? gotoNextPage:null}
+        gotoPrevPage = {prevPageUrl? gotoPrevPage:null}
       />
     </>   
   );
