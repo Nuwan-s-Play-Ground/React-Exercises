eb2e6391f365eb6f6375af2797851dd8ba682130
Nuwan Vidanapathiranage <nuwanpvp@gmail.com>
2024-03-24 19:52:18 +0530

    feat: Add education section and main content

--- React Learning/src/App.jsx
+++ React Learning/src/App.jsx
@@ -5,7 +5,18 @@ function App() {
 
   return (
     <>
-      
+      <nav>
+        <h1>My Education</h1>
+        <ol>
+          <li>Combined Maths</li>
+          <li>Physics</li>
+          <li>Chemistry</li>
+          <li>Software Engineering</li>
+        </ol>
+      </nav>
+      <main>
+        <p>I love to learn Science and Engineering</p>
+      </main>
     </>
   )
 }
