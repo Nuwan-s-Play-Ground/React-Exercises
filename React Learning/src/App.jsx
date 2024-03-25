5a23e335fa1f3adbb6fe724cd60046597f75de27
Nuwan Vidanapathiranage <nuwanpvp@gmail.com>
2024-03-25 17:07:57 +0530

    feat: Create Header component

--- React Learning/src/App.jsx
+++ React Learning/src/App.jsx
@@ -3,12 +3,14 @@ import { useState } from 'react'
 import Footer from './Footer'
 import Navbar from './Navbar'
 import MainContent from './MainContent'
+import Header from './Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Header/>
       <Navbar/>
       <MainContent/>
       <Footer/>
