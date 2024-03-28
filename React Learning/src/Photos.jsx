c671b69b0155dfeb18018ff7c75887e12bb4b4b0
Nuwan Vidanapathiranage <nuwanpvp@gmail.com>
2024-03-28 23:27:20 +0530

    Refactor ContactCards component for better clarity

--- React Learning/src/Photos.jsx
+++ React Learning/src/Photos.jsx
@@ -11,7 +11,20 @@ function Photos() {
         phone="123-456-7890"
         imageUrl="https://www.gstatic.com/webp/gallery/1.jpg"
       />
+      <ContactCards
+      name="Ms. Mary"
+      email="mary@example.com"
+      phone="987-654-3210"
+      imageUrl="https://www.gstatic.com/webp/gallery/2.jpg"
+      />
+      <ContactCards
+      name="Dr. John"
+      email="john@example.com"
+      phone="555-123-4567"
+      imageUrl="https://www.gstatic.com/webp/gallery/3.jpg"
+      />
     </div>
+    
   );
 }
 
