959008835a2d802ded61019569157c3a83e01466
Nuwan Vidanapathiranage <nuwanpvp@gmail.com>
2024-03-29 22:59:14 +0530

    feat: Implement conditional styling in Joke component

--- React Learning/src/Joke.jsx
+++ React Learning/src/Joke.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Joke(props){
     return(
         <div>
-            <h3>Question:{props.question}</h3>
+            <h3 style={props.question ? "block":"none"}>Question:{props.question}</h3>
             <h3>Answer:{props.answer}</h3>
         </div>
     )
