0c97804bba3b4e10496789dd90023994d0121cd9
Nuwan Vidanapathiranage <nuwanpvp@gmail.com>
2024-03-29 22:52:52 +0530

    feat: Create Joke component

--- React Learning/src/App.jsx
+++ React Learning/src/App.jsx
@@ -6,6 +6,7 @@ import MainContent from './MainContent'
 import Header from './Header'
 import MyComponent from './MyComponent'
 import Photos from './Photos'
+import Joke from './Joke'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,6 +18,9 @@ function App() {
       <MyComponent/>
       <MainContent/>
       <Footer/>
+      <Joke
+      question="What is your name?"
+      answer="My Name is Nuwan"/>
       <Photos/>
     </>
   )
