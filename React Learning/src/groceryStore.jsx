90e27bd232d617ce8daac1ad09818e03c9ccec29
Nuwan Vidanapathiranage <nuwanpvp@gmail.com>
2024-03-30 22:18:41 +0530

    feat: Add grocery store items with basic details

---
+++ React Learning/src/groceryStore.jsx
@@ -0,0 +1,28 @@
+const groceryStore = [
+    {
+        id: 1,
+        name: "Apple",
+        price: 1.99,
+        description: "Fresh and crunchy red apples"
+    },
+    {
+        id: 2,
+        name: "Milk",
+        price: 2.49,
+        description: "1 gallon of whole milk"
+    },
+    {
+        id: 3,
+        name: "Bread",
+        price: 3.99,
+        description: "Whole wheat bread loaf"
+    },
+    {
+        id: 4,
+        name: "Banana",
+        price: 0.99,
+        description: "Ripe yellow bananas"
+    }
+];
+
+console.log(groceryStore);
