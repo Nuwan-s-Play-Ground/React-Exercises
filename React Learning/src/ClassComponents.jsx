32bee655d97dad3c260ea2c196815c89f0232fe5
Nuwan Vidanapathiranage <nuwanpvp@gmail.com>
2024-03-31 23:18:05 +0530

    feat: Initialize state in constructor of ClassComponents

--- React Learning/src/ClassComponents.jsx
+++ React Learning/src/ClassComponents.jsx
@@ -2,10 +2,18 @@ import React from "react";
 
 class ClassComponents extends React.Component{
 
+    constructor(){
+        super();
+        this.state={
+            answer:"Yes"
+        }
+    }
+
     render(){
         return(
             <div>
                 <h1>Learning class based components</h1>
+                <h1>Learning State in React</h1>
             </div>
         )
     }
