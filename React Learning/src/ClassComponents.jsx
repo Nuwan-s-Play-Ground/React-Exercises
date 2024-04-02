9e1a1741736ecfcd800ce657be1f2fd6134564ec
Nuwan Vidanapathiranage <nuwanpvp@gmail.com>
2024-04-02 16:13:59 +0530

    feat: Add isLoggedIn state to ClassComponents

--- React Learning/src/ClassComponents.jsx
+++ React Learning/src/ClassComponents.jsx
@@ -8,17 +8,25 @@ class ClassComponents extends React.Component{
         this.state={
             answer:"Yes",
             name:"Nuwan",
-            age:33
+            age:33,
+            isLoggedIn:true
         }
     }
 
     render(){
+        let wordDisplay;
+        if (this.state.isLoggedIn === true){
+            wordDisplay = "in";
+        } else {
+            wordDisplay = "out"
+        }
         return(
             <div>
                 <h1>Learning class based components</h1>
                 <h1>Is 'Learning State in React' important?{this.state.answer}</h1>
                 <p>My name is {this.state.name}.</p>
                 <p>I am {this.state.age} years old.</p>
+                <h1>You are currently logged {wordDisplay}</h1>
             </div>
         )
     }
