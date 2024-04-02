ac1605b862e467e5697e1c57266b9a45b3e7d1ff
Nuwan Vidanapathiranage <nuwanpvp@gmail.com>
2024-04-02 15:43:25 +0530

    feat: Add name and age to state in ClassComponents

--- React Learning/src/ClassComponents.jsx
+++ React Learning/src/ClassComponents.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 
+
 class ClassComponents extends React.Component{
 
     constructor(){
         super();
         this.state={
-            answer:"Yes"
+            answer:"Yes",
+            name:"Nuwan",
+            age:33
         }
     }
 
@@ -14,6 +17,8 @@ class ClassComponents extends React.Component{
             <div>
                 <h1>Learning class based components</h1>
                 <h1>Is 'Learning State in React' important?{this.state.answer}</h1>
+                <p>My name is {this.state.name}.</p>
+                <p>I am {this.state.age} years old.</p>
             </div>
         )
     }
