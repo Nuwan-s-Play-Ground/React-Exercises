00eaf9209974ac1908fb3b96ceace63b38d00c05
Nuwan Vidanapathiranage <nuwanpvp@gmail.com>
2024-04-05 22:32:22 +0530

    feat: Update handleClick method to increment count state in ChangingState component
    In the ChangingState component, modified the handleClick method to use setState with a function to increment the count state by 1. This approach ensures proper state management by using the previous state to calculate the new state value, providing the expected behavior of incrementing the count when the button is clicked.

--- React Learning/src/ChangingState.jsx
+++ React Learning/src/ChangingState.jsx
@@ -10,15 +10,19 @@ class ChangingState extends React.Component{
     }
 
     handleClick(){
-        this.setState({count : 1})
+        this.setState(prevState =>{
+            return{
+                count : prevState.count + 1
+            }
+        })
     }
 
     render(){
         return(
             <div>
-            <h1>{this.state.count}</h1>
-            <button onClick={this.handleClick}>Change</button>
-        </div>
+                <h1>{this.state.count}</h1>
+                <button onClick={this.handleClick}>Change</button>
+            </div>
         )
         
     }
