4bf4a03c661bc264a3cee3733db8068b734aba30
Nuwan Vidanapathiranage <nuwanpvp@gmail.com>
2024-04-10 14:52:31 +0530

    feat: Implement Count component with increment button functionality

--- React Learning/src/Count.jsx
+++ React Learning/src/Count.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 
 class Count extends React.Component{
-    constructor(){
-        super()
+    constructor(props){
+        super(props)
+        this.state = {
+            counter : 0
+        }
+
+        this.increment = () => this.setState({counter : this.state.counter + 1});
     }
 
     render(){
         return(
-            <div className="counter">
-                Counter:{0}
+            <div>
+                <button onClick={this.increment}>Increment</button>
+                <button></button>
+                <div className="counter">
+                Counter:{this.state.counter}
+                </div>
             </div>
         )
     }
