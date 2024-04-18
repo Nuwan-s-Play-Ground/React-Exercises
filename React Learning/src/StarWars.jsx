2b6eb95e4771057384d1a0c2f0ef293fbc592f84
Nuwan Vidanapathiranage <nuwanpvp@gmail.com>
2024-04-18 16:53:40 +0530

    feat: Add loading state and display logic to StarWars component

--- React Learning/src/StarWars.jsx
+++ React Learning/src/StarWars.jsx
@@ -4,24 +4,27 @@ export default class StarWars extends Component {
     constructor(){
         super()
         this.state = {
+            loading:false,
             character:{}
         }
     }
 
     componentDidMount(){
+        this.setState({loading:true})
         fetch("https://swapi.dev/api/people/1")
             .then(Response => Response.json())
             .then(data => {
                 this.setState({
                     character : data
                 })
-            })
+        })
     }
 
     render() {
+        const text = this.state.loading ? "Loading..." : this.state.character.name
         return (
         <div>
-            {this.state.character.name}
+            {text}
         </div>
         )
     }
