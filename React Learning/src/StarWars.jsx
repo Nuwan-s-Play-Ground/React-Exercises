389ff4d09c95d669c3fd6a257338aec935c2787d
Nuwan Vidanapathiranage <nuwanpvp@gmail.com>
2024-04-18 15:50:33 +0530

    feat: Add skeleton of StarWars component

---
+++ React Learning/src/StarWars.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+export default class StarWars extends Component {
+    constructor(){
+        super()
+        this.state = {}
+    }
+
+    componentDidMount(){
+        
+    }
+
+    render() {
+        return (
+        <div>
+            
+        </div>
+        )
+    }
+}
